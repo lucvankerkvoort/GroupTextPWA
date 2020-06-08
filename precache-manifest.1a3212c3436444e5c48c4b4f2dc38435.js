098ca8895ceb3cf9bf3ef7165ea039e7ed4cdeb1
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-06-08 13:35:52 -0700

    Updates

---
+++ precache-manifest.1a3212c3436444e5c48c4b4f2dc38435.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af55d540d92321469df470b13b82ea5",
+    "url": "/GroupTextPWA/index.html"
+  },
+  {
+    "revision": "1682755a997f5c465b15",
+    "url": "/GroupTextPWA/static/css/main.1df9952e.chunk.css"
+  },
+  {
+    "revision": "c20006a1e1e57ecd6c73",
+    "url": "/GroupTextPWA/static/js/2.92f70475.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GroupTextPWA/static/js/2.92f70475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e8dac2fcf94e72632ae",
+    "url": "/GroupTextPWA/static/js/3.192c0767.chunk.js"
+  },
+  {
+    "revision": "3811a61537208e851bc6",
+    "url": "/GroupTextPWA/static/js/4.28181047.chunk.js"
+  },
+  {
+    "revision": "322b6180555be918de1b",
+    "url": "/GroupTextPWA/static/js/5.b33bd4dd.chunk.js"
+  },
+  {
+    "revision": "084b9d7f97fa59137b24",
+    "url": "/GroupTextPWA/static/js/6.0dd0a4a4.chunk.js"
+  },
+  {
+    "revision": "1682755a997f5c465b15",
+    "url": "/GroupTextPWA/static/js/main.4304da8e.chunk.js"
+  },
+  {
+    "revision": "dd31ad6e942ae9e9e6ec",
+    "url": "/GroupTextPWA/static/js/runtime-main.b35b82ba.js"
+  }
+]);
